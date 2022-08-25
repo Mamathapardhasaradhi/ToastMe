cc33dfc2f85ee82af3dc9ad04618c52006c9bb6c
Mamatha <mamatha>
2022-08-25 15:58:21 +0530

    MethodsAdded

--- Sources/ToastMe/ToastMe.swift
+++ Sources/ToastMe/ToastMe.swift
@@ -1,46 +1,45 @@
 import UIKit
-public struct ToastMeStr {
+public struct ToastMe {
     public private(set) var text = "Hello, World!"
 
     public init() {
     }
     
-    public func showHelloWorld() -> String {
-        return "Hello world"
+    public func isValidEmail(emailText: String) -> Bool {
+        if(emailText.count > 0) {
+            var isEmail: Bool {
+                do {
+                    let regex = try NSRegularExpression(pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}", options: .caseInsensitive)
+                    return regex.firstMatch(in: emailText, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, emailText.count)) != nil
+                } catch {
+                    return false
+                }
+            }
+            if(isEmail == true) {
+                return true
+            } else {
+                return false
+            }
+        }
+        return false
     }
     
-    
-}
-
-open class ToastMe: UIView {
-    
-    public private(set) var text = "Hello, World!"
-    
-    public required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    public init() {
-        super.init(frame: CGRect(x: 0, y: 0, width: 320, height: 44))
-        configure()
-    }
-
-    func configure() {
-        // Clear subViews
-        loadViewFromNib()
-    }
-}
-
-extension UIView {
-    func loadViewFromNib() {
-        let nibName = NSStringFromClass(type(of: self)).components(separatedBy: ".").last!
-        let view = Bundle(for: type(of: self)).loadNibNamed(nibName, owner: self, options: nil)?.first as! UIView
-        view.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(view)
-        
-        let views = ["view": view]
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[view]-0-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: views))
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[view]-0-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: views))
-        setNeedsUpdateConstraints()
+    public func isValidMobileNumber(mobileNumberText: String) -> Bool {
+        if(mobileNumberText.count > 0) {
+            var isPhoneNumber: Bool {
+                let PHONE_REGEX = "^[0-9]{8,13}$"//^[789]\\d{9}$
+                let phoneTest = NSPredicate(format: "SELF MATCHES %@", PHONE_REGEX)
+                let result =  phoneTest.evaluate(with: mobileNumberText)
+                return result
+            }
+            if(isPhoneNumber == true) {
+                return true
+            } else {
+                return false
+            }
+        }
+        return false
     }
 }
+
+
